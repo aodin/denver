88ee9e2b8a93b1cbdc82768c4a94818fb3f6dab0
aodin <aaronoellis@gmail.com>
2014-06-15 18:07:29 -0600

    Added a Resources type and versioning for an API

--- api/api.go
+++ api/api.go
@@ -15,6 +15,16 @@ type Resource interface {
 	Get(url.Values) []byte
 }
 
+type Resources map[string]Resource
+
+func (r Resources) URLs(root string) map[string]string {
+	urls := make(map[string]string)
+	for key, _ := range r {
+		urls[key] = fmt.Sprintf("/%s/%s/", root, key)
+	}
+	return urls
+}
+
 type hearingsAPI struct {
 	db *aspect.DB
 }
@@ -29,9 +39,17 @@ func (h *hearingsAPI) Get(parameters url.Values) []byte {
 }
 
 type API struct {
+	version   int
 	db        *aspect.DB
-	resources map[string]Resource
+	resources Resources
 	router    *httprouter.Router
+}
+
+// Resources lists the available resources
+func (a *API) Resources(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	root := fmt.Sprintf("v%d", a.version)
+	b, _ := json.MarshalIndent(a.resources.URLs(root), "", "    ")
+	w.Write(b)
 }
 
 func (a *API) Get(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
@@ -63,13 +81,21 @@ func (a *API) ListenAndServe() error {
 	return http.ListenAndServe(":8080", a.router)
 }
 
+// TODO Root should include all attached APIs
+func Root(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	w.Write([]byte(fmt.Sprintf(`{"version %d": "/v%d/"}`, 1, 1)))
+}
+
 func New(db *aspect.DB) *API {
 	a := &API{
+		version:   1,
 		db:        db,
-		resources: make(map[string]Resource),
+		resources: make(Resources),
 		router:    httprouter.New(),
 	}
+	a.router.GET("/", Root)
+	a.router.GET("/v1/", a.Resources)
 	a.Add("hearings", &hearingsAPI{db: db})
-	a.router.GET("/api/:resource", a.Get)
+	a.router.GET("/v1/:resource", a.Get)
 	return a
 }
