1dc0d5b9f57e1cb8de6cbe089057b25ed353241b
aodin <aaronoellis@gmail.com>
2014-06-16 10:53:57 -0600

    Added ordering to hearings API

--- api/api.go
+++ api/api.go
@@ -9,6 +9,7 @@ import (
 	"github.com/julienschmidt/httprouter"
 	"net/http"
 	"net/url"
+	"strings"
 )
 
 // TODO headers too?
@@ -31,10 +32,37 @@ type hearingsAPI struct {
 }
 
 func (h *hearingsAPI) Get(parameters url.Values) []byte {
+	// Allow ordering from GET parameters
+	order := strings.ToLower(parameters.Get("order"))
+
+	// We also need to remove the minus sign if it exists
+	var inverted bool
+	if order != "" && string(order[0]) == `-` {
+		order = order[1:]
+		inverted = true
+	}
+
+	// Default to "id" ASC
+	_, exists := liquor.Hearings.C[order]
+	if order == "" || !exists {
+		order = "id"
+		inverted = false
+	}
+
+	// Create the order by statements
+	orderBy := liquor.Hearings.C[order].Asc()
+	if inverted {
+		orderBy = orderBy.Desc()
+	}
+
+	// Perform the query and return all results
+	// TODO pagination
+	stmt := liquor.Hearings.Select().OrderBy(orderBy)
 	var hearings []liquor.Hearing
-	stmt := liquor.Hearings.Select().OrderBy(liquor.Hearings.C["id"].Asc())
 	// TODO error is ignored
 	h.db.QueryAll(stmt, &hearings)
+
+	// Convert to pretty printed JSON
 	b, _ := json.MarshalIndent(&hearings, "", "    ")
 	return b
 }
@@ -49,11 +77,13 @@ type API struct {
 
 // Resources lists the available resources
 func (a *API) Resources(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	w.Header().Set("Content-Type", "application/json")
 	root := fmt.Sprintf("v%d", a.version)
 	b, _ := json.MarshalIndent(a.resources.URLs(root), "", "    ")
 	w.Write(b)
 }
 
+// Get will GET the resource with the requested name
 func (a *API) Get(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	// Determine which resource was requested
 	name := ps.ByName("resource")
@@ -62,13 +92,14 @@ func (a *API) Get(w http.ResponseWriter, r *http.Request, ps httprouter.Params) 
 		http.NotFound(w, r)
 		return
 	}
-	// TODO Errors are ignored
+	// Set the content type
+	w.Header().Set("Content-Type", "application/json")
+
 	// Get the parameters
-	// TODO Or r.URL.Query()
-	r.ParseForm()
-	w.Write(resource.Get(r.Form))
+	w.Write(resource.Get(r.URL.Query()))
 }
 
+// Add will add the given resource at the given name
 func (a *API) Add(name string, resource Resource) error {
 	if _, exists := a.resources[name]; exists {
 		return fmt.Errorf("Resource %s already exists", name)
@@ -77,6 +108,7 @@ func (a *API) Add(name string, resource Resource) error {
 	return nil
 }
 
+// ListenAndServe will start the API server and run forever
 func (a *API) ListenAndServe() error {
 	address := fmt.Sprintf(":%d", a.config.Port)
 	fmt.Printf("Starting on %s\n", address)
@@ -85,9 +117,14 @@ func (a *API) ListenAndServe() error {
 
 // TODO Root should include all attached APIs
 func Root(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	w.Write([]byte(fmt.Sprintf(`{"version %d": "/v%d/"}`, 1, 1)))
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte(fmt.Sprintf(`{
+    "version %d": "/v%d/"
+}`, 1, 1)))
 }
 
+// New will create a new API instance
+// TODO A global registry for API versions
 func New(c config.Config, db *aspect.DB) *API {
 	a := &API{
 		version:   1,
@@ -99,6 +136,7 @@ func New(c config.Config, db *aspect.DB) *API {
 	a.router.GET("/", Root)
 	a.router.GET("/v1/", a.Resources)
 	a.Add("hearings", &hearingsAPI{db: db})
-	a.router.GET("/v1/:resource", a.Get)
+	// TODO This should be added automatically
+	a.router.GET("/v1/:resource/", a.Get)
 	return a
 }
